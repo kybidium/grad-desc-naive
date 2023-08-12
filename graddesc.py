f744773b2453285c1eede1e3384f9385b8632cbe
Luis Ojeda <84539145+kybidium@users.noreply.github.com>
2023-08-12 01:27:26 -0400

    Update graddesc.py
    Adding version that works--imported from school acct (@luisojedacs)

--- graddesc.py
+++ graddesc.py
@@ -1,160 +1,203 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-import random
-import statistics
 
 # goal: find best fit polynomial for values of sin
 
 def integ(fn, lb, ub, N):
-	"""
-	From Computer Science with Applications by borya and rogers
-	"""
+    """
+    Integral function from the textbook Computer Science with Applications by 
+    Professors Borja Sotomayor and Anne Rogers. Approximates an integral with a 
+    midpoint Riemann Sum.
 
-	delta = (ub - lb)/N
-	sum = 0
-	for i in range(N):
-    	sum += delta * fn(lb + delta * (i + 0.5))
-	return sum
+    Inputs:
+        fn (function): the function whose integral will be taken
+        lb (int): the lower bound of the integral
+        ub (int): the upper bound of the integral
+        N (int): the number of partitions
+
+    Returns: the integral of the function fn from the lower bound
+        to the upper bound, approximated with N partitions
+    """
+
+    delta = (ub - lb)/N 
+    sum = 0
+    for i in range(N):
+        sum += delta * fn(lb + delta * (i + 0.5))
+    return sum
 
 def least_squares(fn1, fn2):
-	"""
-	From Khanacademy: takes two functions, finds least squares
-	difference between them at a given input
-	"""
-	def leastsqx(inp):
-    	"""
-    	Returns w input
-    	"""
-    	return (fn1(inp) - fn2(inp))**2
-	return leastsqx
+    """
+    Returns a function evaluating the least-squares difference bewteen two
+    functions
 
-	return (a0 + a1*x + a2*x**2 + a3*x**3 + a4*x**4 + a5*x**5)
-def random_poly():
-	"""
-	generate random polynomial
-	"""
-	a0, a1, a2, a3, a4, a5 = random.randrange(10), random.randrange(10), random.randrange(10), random.randrange(10), random.randrange(10), random.randrange(10)
-	def randompolx(x):
-    	return (a0 + a1*x + a2*x**2 + a3*x**3 + a4*x**4 + a5*x**5)
-	return randompolx
+    Inputs:
+        fn1 (function): the first function
+        fn2 (function): the second function
+    
+    Returns: a function evaluating the least-squares difference between fn1 and
+        fn2.
+    """
+    def leastsqx(x):
+        """
+        Evaluates the least-squares function at a specific input x
 
-rando = random_poly()
-#create specific instance of random poly
+        Input:
+            x (int): an input value
+        
+        Returns: the least-squares function evaluated at input x
+        """
+        return (fn1(x) - fn2(x))**2
+    return leastsqx
 
 #maybe abstract this one even more
 def polynomial_approx(a0, a1, a2, a3, a4, a5):
-	"""
-	From Khanacademy: approximates polynomial with constants and
-	x val
-	"""
-	def polapproxx(x):
-    	return (a0 + a1*x + a2*x**2 + a3*x**3 + a4*x**4 + a5*x**5)
-	return polapproxx
+    """
+    Inspired by Khan Academy function--a polynomial that approximates a 
+    function using coefficients a0 through a5.
+
+    Inputs:
+        a0...a5 (int): the coefficients of the polynomial
+    
+    Returns: a function to calculate the polynomial for a given input
+    """
+    def polapproxx(x):
+        """
+        Evaluates the polynomial for an input x
+
+        Input:
+            x (int): the input value for the polynomial
+        
+        Returns: the polynomial evaluated for input x
+        """
+        return (a0 + a1*x + a2*x**2 + a3*x**3 + a4*x**4 + a5*x**5)
+
+    return polapproxx
 
 
-def grad_desc_original(lb, ub, N, a0, a1, a2, a3, a4, a5):
-	"""
-	From Khanacademy: least squares integral
-	"""
-	result = integ(least_squares(polynomial_approx(a0, a1, a2, a3, a4, a5),
-    	pol_secure), lb, ub, N)
-	return result
+def least_squares_integral(lb, ub, N, a0, a1, a2, a3, a4, a5):
+    """
+    The least-squares integral from Khan Academy generalized for any upper or
+    lower bound. Integrates the least-squares difference function
+    of two functions from a lower bound to an upper bound. Specified for
+    polynomial approximation of sin(x) and sin(x)--but easily generalizable.
+
+    Inputs:
+        lb (int): the lower bound of the integral
+        ub (int): the upper bound of the integral
+        N (int): the number of partitions for the integral
+        a0...a5 (int): the coefficients of the polynomial
+
+    Returns: the least-squares integral from lb to ub of sin(x) and its
+        approximation with N partitions.
+    """
+    result = integ(least_squares(polynomial_approx(a0, a1, a2, a3, a4, a5),
+        math.sin), lb, ub, N)
+    return result
 
 def grad_desc_actual(lb, ub, N, a0, a1, a2, a3, a4, a5, inc, tol):
-	"""
-	Own attempt
-	"""
-	i = 100
-	while (grad_desc_original(lb, ub, N, a0, a1, a2, a3, a4, a5) > tol):
-    	# a0
-    	if (grad_desc_original(lb, ub, N, a0-inc, a1, a2, a3, a4, a5)
-        	< grad_desc_original(lb, ub, N, a0, a1, a2, a3, a4, a5)):
-        	a0 -= inc
-    	elif (grad_desc_original(lb, ub, N, a0+inc, a1, a2, a3, a4, a5)
-        	< grad_desc_original(lb, ub, N, a0, a1, a2, a3, a4, a5)):
-        	a0 += inc
+    """
+    A rudimentary form of gradient descent--checks what the effect
+    would be of increasing or decreasing each coefficient on the value of the
+    least-squares integral function. If a decrement decreases the value,
+    the variable is decremented; if an increment increases it, the variable is
+    incremented. Runs until the least-squares integral falls below the maximum
+    acceptable value, then returns the coefficients.
 
-    	print(f"a0: {a0}\n")
+    Inputs:
+        lb (int): the lower bound of the least-squares intgeral
+        ub (int): the upper bound of the least-squares integral
+        N (int): the number of partitions of the least-squares integral
+        a0...a5 (int): the coefficients of the polynomial
+        inc (int): the value to (de/in)crement the coefficients by
+        tol (int): the maximum acceptable value for the least-square integral
+    """
+    while (least_squares_integral(lb, ub, N, a0, a1, a2, a3, a4, a5) > tol):
+        # a0
+        if (least_squares_integral(lb, ub, N, a0-inc, a1, a2, a3, a4, a5)
+            < least_squares_integral(lb, ub, N, a0, a1, a2, a3, a4, a5)):
+            a0 -= inc
+        elif (least_squares_integral(lb, ub, N, a0+inc, a1, a2, a3, a4, a5)
+            < least_squares_integral(lb, ub, N, a0, a1, a2, a3, a4, a5)):
+            a0 += inc
+        print(f"a0: {a0}\n")
+        #   this is repeated below for each coefficient ai
+    # returns a list of the coefficients
 
-    	#a1
-    	if (grad_desc_original(lb, ub, N, a0, a1-inc, a2, a3, a4, a5)
-        	< grad_desc_original(lb, ub, N, a0, a1, a2, a3, a4, a5)):
-        	a1 -= inc
-    	elif (grad_desc_original(lb, ub, N, a0, a1+inc, a2, a3, a4, a5)
-        	< grad_desc_original(lb, ub, N, a0, a1, a2, a3, a4, a5)):
-        	a1 += inc
-    	print(f"a1: {a1}\n")
+        #a1
+        if (least_squares_integral(lb, ub, N, a0, a1-inc, a2, a3, a4, a5)
+            < least_squares_integral(lb, ub, N, a0, a1, a2, a3, a4, a5)):
+            a1 -= inc
+        elif (least_squares_integral(lb, ub, N, a0, a1+inc, a2, a3, a4, a5)
+            < least_squares_integral(lb, ub, N, a0, a1, a2, a3, a4, a5)):
+            a1 += inc
+        print(f"a1: {a1}\n")
 
-    	#a2
-    	if (grad_desc_original(lb, ub, N, a0, a1, a2-inc, a3, a4, a5)
-        	< grad_desc_original(lb, ub, N, a0, a1, a2, a3, a4, a5)):
-        	a2 -= inc
-    	elif (grad_desc_original(lb, ub, N, a0, a1, a2+inc, a3, a4, a5)
-        	< grad_desc_original(lb, ub, N, a0, a1, a2, a3, a4, a5)):
-        	a2 += inc
-    	print(f"a2: {a2}\n")
+        #a2
+        if (least_squares_integral(lb, ub, N, a0, a1, a2-inc, a3, a4, a5)
+            < least_squares_integral(lb, ub, N, a0, a1, a2, a3, a4, a5)):
+            a2 -= inc
+        elif (least_squares_integral(lb, ub, N, a0, a1, a2+inc, a3, a4, a5)
+            < least_squares_integral(lb, ub, N, a0, a1, a2, a3, a4, a5)):
+            a2 += inc
+        print(f"a2: {a2}\n")
 
-    	#a3
-    	if (grad_desc_original(lb, ub, N, a0, a1, a2, a3-inc, a4, a5)
-        	< grad_desc_original(lb, ub, N, a0, a1, a2, a3, a4, a5)):
-        	a3 -= inc
-    	elif (grad_desc_original(lb, ub, N, a0, a1, a2, a3+inc, a4, a5)
-        	< grad_desc_original(lb, ub, N, a0, a1, a2, a3, a4, a5)):
-        	a3 += inc
-    	print(f"a3: {a3}\n")
+        #a3
+        if (least_squares_integral(lb, ub, N, a0, a1, a2, a3-inc, a4, a5)
+            < least_squares_integral(lb, ub, N, a0, a1, a2, a3, a4, a5)):
+            a3 -= inc
+        elif (least_squares_integral(lb, ub, N, a0, a1, a2, a3+inc, a4, a5)
+            < least_squares_integral(lb, ub, N, a0, a1, a2, a3, a4, a5)):
+            a3 += inc
+        print(f"a3: {a3}\n")
 
-    	#a4
-    	if (grad_desc_original(lb, ub, N, a0, a1, a2, a3, a4-inc, a5)
-        	< grad_desc_original(lb, ub, N, a0, a1, a2, a3, a4, a5)):
-        	a4 -= inc
-    	elif (grad_desc_original(lb, ub, N, a0, a1, a2, a3, a4+inc, a5)
-        	< grad_desc_original(lb, ub, N, a0, a1, a2, a3, a4, a5)):
-        	a4 += inc
-    	print(f"a4: {a4}\n")
+        #a4
+        if (least_squares_integral(lb, ub, N, a0, a1, a2, a3, a4-inc, a5)
+            < least_squares_integral(lb, ub, N, a0, a1, a2, a3, a4, a5)):
+            a4 -= inc
+        elif (least_squares_integral(lb, ub, N, a0, a1, a2, a3, a4+inc, a5)
+            < least_squares_integral(lb, ub, N, a0, a1, a2, a3, a4, a5)):
+            a4 += inc
+        print(f"a4: {a4}\n")
 
-    	#a5
-    	if (grad_desc_original(lb, ub, N, a0, a1, a2, a3, a4, a5-inc)
-        	< grad_desc_original(lb, ub, N, a0, a1, a2, a3, a4, a5)):
-        	a5 -= inc
-    	elif (grad_desc_original(lb, ub, N, a0, a1, a2, a3, a4, a5+inc)
-        	< grad_desc_original(lb, ub, N, a0, a1, a2, a3, a4, a5)):
-        	a5 += inc
-    	print(f"a5: {a5}\n")
-    	i += 1
-	return [a0, a1, a2, a3, a4, a5]
-
-def pol_secure(x):
-
-	return (2 + 3*x + 6*x**2 + 2*x**3 + 8*x**4 + 5*x**5)
+        #a5
+        if (least_squares_integral(lb, ub, N, a0, a1, a2, a3, a4, a5-inc)
+            < least_squares_integral(lb, ub, N, a0, a1, a2, a3, a4, a5)):
+            a5 -= inc
+        elif (least_squares_integral(lb, ub, N, a0, a1, a2, a3, a4, a5+inc)
+            < least_squares_integral(lb, ub, N, a0, a1, a2, a3, a4, a5)):
+            a5 += inc
+        print(f"a5: {a5}\n")
+    return [a0, a1, a2, a3, a4, a5]
 
 def grad_descend_recurs(lb, ub, N, a0, a1, a2, a3, a4, a5):
-	"""
-	recurses grad desc
-	"""
-	tol = statistics.mean([abs(pol_secure(-3)),abs(pol_secure(-2)),abs(pol_secure(0)),abs(pol_secure(2)),abs(pol_secure(3))])/100
-	inc = tol/1000
-	toltol = 0.01*tol
-	while (tol > toltol):
-    	avals = grad_desc_actual(lb, ub, N, a0, a1, a2, a3, a4, a5, inc, tol)
-    	a0, a1, a2, a3, a4, a5 = avals
-    	inc *= 0.9
-    	tol *= 0.9
-    	print(inc, tol)
-	return avals
-
-
+    """
+    recurses grad desc
+    """
+    inc = 1
+    tol = 100
+    inctol = 0.001
+    toltol = 0.01
+    while (tol > toltol):
+        avals = grad_desc_actual(lb, ub, N, a0, a1, a2, a3, a4, a5, inc, tol)
+        a0, a1, a2, a3, a4, a5 = avals
+        inc *= 0.9
+        tol *= 0.9
+        print(inc, tol)
+    return avals
 
 valsa = grad_descend_recurs(-3,3,10000,1,2,3,4,5,6)
 print(valsa)
 x = np.linspace(-3, 3, 100)
 
-#pass x as input to polynomial approx for the a values found
-y1 = np.array([polynomial_approx(valsa[0], valsa[1], valsa[2], valsa[3],
-	valsa[4], valsa[5])(xi) for xi in x])
+# valsa: list of values returned by grad_desc_actual
 
-y2 = np.array([pol_secure(xi) for xi in x])
+# generate array of values of the approximation on [-3, 3]
+y1 = np.array([polynomial_approx(valsa[0], valsa[1], valsa[2], valsa[3], 
+    valsa[4], valsa[5])(xi) for xi in x])
+
+#generate the actual values of sin(x) on [-3, 3]
+y2 = np.array([math.sin(xi) for xi in x])
 
 plt.plot(x, y1, 'r--', x, y2, 'b--')
 plt.savefig('plot.png')
-
